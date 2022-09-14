9be44569437799b21ad15b8b7d2a72fbdd23a4bc
Abdsmadi4 <abd.smadi4@gmail.com>
2022-09-14 13:33:51 +0300

    Insert at the beginning of LL

--- ll.js
+++ ll.js
@@ -6,6 +6,16 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
+
+  insertAtBeginning(value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  }
 }
 
 module.exports = LinkedList;
