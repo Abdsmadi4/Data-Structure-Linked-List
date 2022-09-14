9cdb890fa45e8197c819f884705ae7be101331ac
Abdsmadi4 <abd.smadi4@gmail.com>
2022-09-14 15:52:36 +0300

    Delete the middle element from the LL

--- ll.js
+++ ll.js
@@ -7,6 +7,16 @@ class LinkedList {
     this.head = null;
   }
 
+  size() {
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count++;
+      node = node.next;
+    }
+    return count;
+  }
+
   insertAtBeginning(value) {
     let newNode = new Node(value);
     if (!this.head) {
@@ -16,8 +26,8 @@ class LinkedList {
       this.head = newNode;
     }
   }
-  
-    insertAtLast(value) {
+
+  insertAtLast(value) {
     let newNode = new Node(value);
     if (!this.head) {
       this.head = newNode;
@@ -29,6 +39,39 @@ class LinkedList {
       currentNode.next = newNode;
     }
   }
+
+  deleteAtMiddle() {
+    if (this.size() === 0) {
+      return null;
+    }
+    if (this.size() === 1) {
+      this.head = null;
+      return;
+    } else if (this.size() === 2) {
+      this.head.next = null;
+      return;
+    } else if (this.size() > 2 && this.size() % 2 === 0) {
+      let count = this.size() / 2;
+      let currentNode = this.head;
+      while (count - 1) {
+        currentNode = currentNode.next;
+        count--;
+      }
+      currentNode.next = currentNode.next.next;
+      return;
+    } else if (this.size() > 2 && this.size() % 2 !== 0) {
+      let count = this.size() / 2 + 0.5;
+      let currentNode = this.head;
+      while (count - 2) {
+        currentNode = currentNode.next;
+        count--;
+      }
+      currentNode.next = currentNode.next.next;
+    } else {
+      return;
+    }
+  }
+  
 }
 
 module.exports = LinkedList;
